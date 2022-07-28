cad78ca3a44f8da864cf359178d516f021fcc8b3
Elie Zedeck RANDRIAMIANDRIRAY <elie.zedeck@gmail.com>
2022-07-28 13:50:39 +0300

    Basic Webserver and logging features

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/eliezedeck/webhook-ingestor
+
+go 1.18
+
+require (
+	github.com/eliezedeck/gobase v0.12.5 // indirect
+	github.com/labstack/echo/v4 v4.7.2 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/net v0.0.0-20220726230323-06994584191e // indirect
+	golang.org/x/sys v0.0.0-20220727055044-e65921a090b8 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/eliezedeck/gobase/logging"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// Setup logging
+	logging.Init()
+	logging.L = logging.L.Named("WebhookIngestor")
+
+	// Setup Web server (using Echo)
+	e := echo.New()
+	e.HidePort = true
+	e.HideBanner = true
+	e.Use(logging.ZapLoggerForEcho(logging.L))
+	e.Use(logging.RecoverWithZapLogging)
+	e.HTTPErrorHandler = func(err error, c echo.Context) {
+		if c.Response().Committed {
+			return
+		}
+		if c.Request().Method == http.MethodHead {
+			c.NoContent(http.StatusInternalServerError)
+			return
+		}
+		erro := c.JSON(http.StatusInternalServerError, 500)
+		if erro != nil {
+			e.Logger.Error(erro)
+		}
+	}
+
+	panic(e.Start(":8080"))
+}
