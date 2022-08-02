e70a47f6df202f2ace1500e2ce095ddb24f03bcd
Elie Zedeck RANDRIAMIANDRIRAY <elie.zedeck@gmail.com>
2022-08-02 11:42:36 +0300

    Ability to specify the -storage to use (memory / mongo)

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/eliezedeck/gobase/logging"
 	"github.com/eliezedeck/webhook-ingestor/core"
 	"github.com/eliezedeck/webhook-ingestor/impl"
+	mongodbimpl "github.com/eliezedeck/webhook-ingestor/impl/mongodb"
 	"github.com/eliezedeck/webhook-ingestor/parameters"
 	"github.com/labstack/echo/v4"
 	"go.uber.org/zap"
@@ -23,19 +24,38 @@ func main() {
 	e := buildEcho()
 
 	// Setup MemoryStorage instance
-	// TODO: This will be configurable in the future
-	storage := impl.NewMemoryStorage()
+	var (
+		configStorage core.ConfigStorage
+		reqStorage    core.RequestsStorage
+	)
+	switch parameters.ParamStorage {
+	case "memory":
+		storage := impl.NewMemoryStorage()
+		configStorage = storage
+		reqStorage = storage
+		logging.L.Info("Using in-memory storage")
+	case "mongo":
+		storage, err := mongodbimpl.NewStorage(parameters.ParamStorageMongoUri, parameters.ParamStorageMongoDb)
+		if err != nil {
+			panic(err)
+		}
+		configStorage = storage
+		reqStorage = storage
+		logging.L.Info("Using MongoDB as storage")
+	default:
+		panic("invalid -storage parameter, valid values are 'memory' and 'mongo'")
+	}
 
 	// -----------
-	setupWebhookPaths(e, storage, storage)
+	setupWebhookPaths(e, configStorage, reqStorage)
 
 	// -----------
 	// Set up the Admin paths
 	if parameters.ParamListen == parameters.ParamAdminListen {
-		core.SetupAdministration(e, storage, storage, parameters.ParamAdminPath)
+		core.SetupAdministration(e, configStorage, reqStorage, parameters.ParamAdminPath)
 	} else {
 		a := buildEcho()
-		core.SetupAdministration(a, storage, storage, parameters.ParamAdminPath)
+		core.SetupAdministration(a, configStorage, reqStorage, parameters.ParamAdminPath)
 		go func() {
 			panic(a.Start(parameters.ParamListen))
 		}()
--- parameters/cli.go
+++ parameters/cli.go
@@ -5,11 +5,16 @@ import (
 )
 
 var (
-	ParamListen        = ":8080"
+	ParamListen = ":8080"
+
 	ParamAdminListen   = ":8081"
 	ParamAdminUsername = "admin"
 	ParamAdminPassword = "admin"
 	ParamAdminPath     = "__admin__"
+
+	ParamStorage         = "memory"
+	ParamStorageMongoUri = "mongodb://localhost:27017"
+	ParamStorageMongoDb  = "webhook-ingestor"
 )
 
 func ParseFlags() {
@@ -18,5 +23,8 @@ func ParseFlags() {
 	flag.StringVar(&ParamAdminUsername, "username", ParamAdminUsername, "Username for admin; defaults to 'admin'")
 	flag.StringVar(&ParamAdminPassword, "password", ParamAdminPassword, "Password for admin; defaults to 'admin'")
 	flag.StringVar(&ParamAdminPath, "admin-path", ParamAdminPath, "Path for admin; defaults to '__admin__'")
+	flag.StringVar(&ParamStorage, "storage", ParamStorage, "Storage type; defaults to 'memory'")
+	flag.StringVar(&ParamStorageMongoUri, "mongo-uri", ParamStorageMongoUri, "MongoDB URI; defaults to 'mongodb://localhost:27017'")
+	flag.StringVar(&ParamStorageMongoDb, "mongo-db", ParamStorageMongoDb, "MongoDB database to use; defaults to 'webhook-ingestor'")
 	flag.Parse()
 }
